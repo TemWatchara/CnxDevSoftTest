c8832778ff4fb8f18151ea69fdcb7651aa05eb15
February <February@DESKTOP-1FGI4B3>
2023-10-09 00:39:32 +0700

    Add project files.

---
+++ CnxDevSoftTest/Models/CnxModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CnxDevSoftTest.Models
+{
+    public class CnxModel
+    {
+        public int? number1 { get; set; }
+        public int? number2 { get; set; }
+        public string? math { get; set; }
+        public double? result { get; set; }
+    }
+}
---
+++ CnxDevSoftTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CnxDevSoftTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
